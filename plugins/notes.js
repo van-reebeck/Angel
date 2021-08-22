62584e2cc7ba081198b48c6852d18b4ef710fcf4
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-22 22:43:04 +0530

    New Forwarded Mark to Notes ✅️

--- plugins/notes.js
+++ plugins/notes.js
@@ -18,16 +18,16 @@ WhatsAlexa.addCommand({ pattern: 'notes', fromMe: true, desc: Lang.NOTES_USAGE }
     })
 
     if (notes.length < 1) {
-        return await message.sendMessage(Lang.NO_SAVED)
+        return await message.sendMessage(message.jid, Lang.NO_SAVED, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
     }
 
-    await message.sendMessage(Lang.SAVED)
+    await message.sendMessage(message.jid, Lang.SAVED, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
 
     await message.sendMessage(notes.join('\n\n'))
     _notes.filter(note => note.note.includes('IMG;;;')).forEach(async (note) => {
         const imageName = note.note.replace('IMG;;;', '')
         const image = await fs.readFile(path.resolve('media', imageName))
-        await message.sendMessage(image, MessageType.image)
+        await message.sendMessage(message.jid, image, MessageType.image, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
     })
 
 
@@ -40,14 +40,14 @@ WhatsAlexa.addCommand({ pattern: 'save ?(.*)', fromMe: true, desc: Lang.SAVE_USA
     const userNote = match[1]
 
     if (!userNote && !message.reply_message) {
-        await message.sendMessage(Lang.REPLY)
+        await message.sendMessage(message.jid, Lang.REPLY, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
 
         return
     }
 
     if (userNote) {
         await NotesDB.saveNote(userNote)
-        await message.sendMessage(Lang.SUCCESSFULLY_ADDED)
+        await message.sendMessage(message.jid, Lang.SUCCESSFULLY_ADDED, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
 
         return
 
@@ -66,18 +66,18 @@ WhatsAlexa.addCommand({ pattern: 'save ?(.*)', fromMe: true, desc: Lang.SAVE_USA
                 const randomFileName = savedFileName.split('.')[0] + Math.floor(Math.random() * 50) + path.extname(savedFileName)
                 await fs.copyFile(savedFileName, path.resolve('media', randomFileName))
                 await NotesDB.saveNote("IMG;;;" + randomFileName)
-                await message.sendMessage(Lang.SUCCESSFULLY_ADDED)
+                await message.sendMessage(message.jid, Lang.SUCCESSFULLY_ADDED, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
 
 
             }
 
             await NotesDB.saveNote(message.reply_message.text)
-            await message.sendMessage(Lang.SUCCESSFULLY_ADDED)
+            await message.sendMessage(message.jid, Lang.SUCCESSFULLY_ADDED, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
 
             return
         }
     } else {
-        await message.sendMessage(Lang.UNSUCCESSFUL)
+        await message.sendMessage(message.jid, Lang.UNSUCCESSFUL, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
 
         return
     }
@@ -93,6 +93,6 @@ WhatsAlexa.addCommand({ pattern: 'deleteNotes', fromMe: true, desc: Lang.DELETE_
         await fs.unlink(path.resolve('media', file))
     })
 
-    return await message.sendMessage(Lang.SUCCESSFULLY_DELETED)
+    return await message.sendMessage(message.jid, Lang.SUCCESSFULLY_DELETED, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
 })
 
