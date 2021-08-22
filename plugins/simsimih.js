4afcb1434b5e9c43b2ebdd290448d57210fb3e9b
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-22 23:23:23 +0530

    New Forward Mark to Simi ✅️

--- plugins/simsimih.js
+++ plugins/simsimih.js
@@ -7,13 +7,13 @@ let Language = require('../language');
 let Lang = Language.getString('ai');
 
 WhatsAlexa.addCommand({pattern: 'simi ?(.*)', fromMe: td, desc: Lang.SIMI_DESC}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.INVALID_REQ);
+	if (match[1] === 'xx') return await message.client.sendMessage(message.jid, Lang.INVALID_REQ, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
 	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*🤖 ' + Lang.BOT_DIVIDER +'* ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUND_RESPONSE, MessageType.text);
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUND_RESPONSE, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }})
 	}
 });
